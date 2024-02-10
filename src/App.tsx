d42195ea80ca89f34337f196ffa0fe3419cee4b8
Mahmodul Hasan <nicesanto92@gmail.com>
2024-02-10 14:25:32 +0600

    netlify bug fixing In App.tsx useState called but not used

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
